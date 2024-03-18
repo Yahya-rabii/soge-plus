b3d18b060607ab6cc0db1a5b46f596019ae78935
Yahya-rabii <rabiiyahya1@gmail.com>
2024-03-18 10:59:16 +0000

    Adding front // org arch

---
+++ AppCC-back/Client/src/main/java/com/sgma/client/services/ClientService.java
@@ -0,0 +1,51 @@
+package com.sgma.client.services;
+
+
+import com.sgma.client.entities.Client;
+import com.sgma.client.Model.Contract;
+import com.sgma.client.repositories.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ClientService {
+    private final ClientRepository clientRepository;
+    private final ContractFetchingService contractService;
+
+    public ClientService(ClientRepository clientRepository, ContractFetchingService contractService) {
+        this.clientRepository = clientRepository;
+        this.contractService = contractService;
+    }
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client getClientById(Long id) {
+        return clientRepository.findById(id).orElse(null);
+    }
+
+    public Client addClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public Client updateClient(Long id, Client client) {
+        client.setId(id);
+        return clientRepository.save(client);
+    }
+
+    public void deleteClient(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+    public List<Contract> getContracts(Long id) {
+        Client client = clientRepository.findById(id).orElse(null);
+        if (client != null) {
+            return contractService.findContractByClientId(id);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+}
