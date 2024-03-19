525d6ee722222d8ac8a44a13693f555c295e9420
salaheddineS <SALAHeddinemorchid1@gmail.com>
2024-03-19 12:52:48 +0000

    [CLEAN] cleaning

--- AppCC-back/Client/src/main/resources/db/migration/V1__create_client_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `client` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(50),
-    `email` varchar(50),
-    `phone` varchar(50)
-
-);
-
--- AppCC-back/Contract/src/main/resources/db/migration/V2__insert_data_to_contract.sql
+++
@@ -1,7 +0,0 @@
--- insert fake data to contract table on startup
-INSERT INTO contract (type, start_date, duration,client_id) VALUES ('CDI', '2020-01-01 00:00:00', 24,1);
-INSERT INTO contract (type, start_date, duration,client_id) VALUES ('CDD', '2020-01-01 00:00:00', 12,1);
-INSERT INTO contract (type, start_date, duration,client_id) VALUES ('CDD', '2020-01-01 00:00:00', 6,2);
-INSERT INTO contract (type, start_date, duration,client_id) VALUES ('CDI', '2020-01-01 00:00:00', 36,2);
-INSERT INTO contract (type, start_date, duration,client_id) VALUES ('INTERN', '2020-01-01 00:00:00', 48,3);
-INSERT INTO contract (type, start_date, duration,client_id) VALUES ('ANAPEC', '2020-01-01 00:00:00', 24,3);
