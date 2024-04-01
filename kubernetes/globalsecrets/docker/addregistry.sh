1c81b68e2f62193f8832249646e5fd1d88151918
salaheddineS <SALAHeddinemorchid1@gmail.com>
2024-04-01 15:45:08 +0000

    [KUBERNETES] Added required ressources for argocd and kube-prometheus operator

--- kubernetes/globalsecrets/docker/addregistry.sh
+++ kubernetes/globalsecrets/docker/addregistry.sh
@@ -2,3 +2,4 @@ kubectl create secret docker-registry regcred \
     --docker-username=salaheddine122 \
     --docker-password=${token} \
     --docker-email=salaheddinemorchid1@gmail.com
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
