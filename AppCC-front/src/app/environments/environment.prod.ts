64a48d518d0f1b4e8d85b7e67efdae05cb175df8
Morchid Salaheddine <89920306+SalaheddineS@users.noreply.github.com>
2024-04-22 13:32:01 +0100

    Update environment.prod.ts

--- AppCC-front/src/app/environments/environment.prod.ts
+++ AppCC-front/src/app/environments/environment.prod.ts
@@ -1,3 +1,62 @@
 export const environment = {
-  production: true
-};
+    production: true,
+    
+    // auth microservice
+
+    AuthapiUrl: 'http://minikube-gateway.socgen.com/auth-service',
+    loginEndpoint: '/login',
+    logoutEndpoint: '/logout',
+    signupEndpoint: '/signup',
+    refreshEndpoint: '/refresh',
+    getRolesEndpoint: '/role',
+
+
+    // client microservice
+
+    ClientMsUrl: 'http://minikube-gateway.socgen.com/client-service',
+    getAllclientsEndpoint: '/clients',
+    getClientByIdEndpoint: '/client/',
+    createClientEndpoint: '/addClient',
+    updateClientEndpoint: '/updateClient/',
+    deleteClientEndpoint: '/deleteClient/',
+    getcontractsByClientIdEndpoint: '/contracts/',
+
+
+    // loan microservice
+
+    LoanMsUrl: 'http://minikube-gateway.socgen.com/loan-service',
+    getAllLoansEndpoint: '/loans',
+    getLoanByIdEndpoint: '/loan/',
+    createLoanEndpoint: '/createLoan',
+    updateLoanEndpoint: '/updateLoan/',
+    deleteLoanEndpoint: '/deleteLoan/',
+    getLoansByClientIdEndpoint: '/loanByClientId/',
+    validateLoanEndpoint: '/validateLoan/',
+    rejectLoanEndpoint: '/rejectLoan/',
+
+    // contract microservice
+
+    ContractMsUrl: 'http://minikube-gateway.socgen.com/contract-service',
+    getAllContractsEndpoint: '/contracts',
+    getContractByIdEndpoint: '/contract/',
+    createContractEndpoint: '/createContract',
+    updateContractEndpoint: '/updateContract/',
+    deleteContractEndpoint: '/deleteContract/',
+    getContractsOfClientEndpoint: '/contracts/client/',
+
+
+    // Account microservice
+    AccountMsUrl: 'http://minikube-gateway.socgen.com/account-service',
+    getAllAccountsEndpoint: '/Accounts',
+    getAccountByIdEndpoint: '/Account/',
+    createAccountEndpoint: '/createAccount',
+    updateAccountEndpoint: '/updateAccount/',
+    deleteAccountEndpoint: '/deleteAccount/',
+    getAccountByAccountHolderIdEndpoint: '/Accounts/client/',
+    getBeneficiariesEndpoint: '/beneficiaries/',
+    addBeneficiaryEndpoint: '/beneficiary/',
+
+  };
+
+
+  
