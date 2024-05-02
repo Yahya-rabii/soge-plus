b1bb5edbf737c1ac782113e60f13ea26518c74c8
salaheddineS <SALAHeddinemorchid1@gmail.com>
2024-05-02 12:40:25 +0100

    [ADMIN-SERVICE] removed temporaryly compatibility for cloud native dashboard

--- AppCC-back/Admin/src/main/java/com/sgma/admin/AdminApplication.java
+++ AppCC-back/Admin/src/main/java/com/sgma/admin/AdminApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableScheduling
 @EnableAdminServer
 public class AdminApplication {
     public static void main(String[] args) {
