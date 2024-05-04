882168f1ea70a539aaf899c08cbf53330e98d2a6
Yahya-rabii <rabiiyahya1@gmail.com>
2024-05-04 21:13:57 +0100

    refractoring V1.0.0

--- AppCC-back/Contract/src/main/java/com/sgma/contract/services/LoanFetchingService.java
+++ AppCC-back/Contract/src/main/java/com/sgma/contract/services/LoanFetchingService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "LOAN-SERVICE")
+@FeignClient(name = "loan-service")
 public interface LoanFetchingService {
     @GetMapping(path = "/loanByClientId/{clientId}")
     List<Loan> getLoansByClientId(@PathVariable("clientId") String clientId);
